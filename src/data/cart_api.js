9d5aca628760cdf62dfd4bb31de0ccc70a770b8d
efhassman <ehassman@g.hmc.edu>
2021-01-12 20:24:55 -0600

    basic cart api structure, def can change

--- src/data/cart_api.js
+++ src/data/cart_api.js
@@ -1,29 +1,82 @@
 const api = [
     {
-        product: {
-            id: "",
-            name: "Contour Concealer",
-            price: "69",
-            image: "",
-            quantity: "",
-            Brand: {
-                id: "",
-                name: "Ulta Beauty",
+        title: "cart",
+        total: 1446,
+        products: [
+            {
+                id: "0",
+                quantity: 1,
+                product: {
+                    id: "",
+                    name: "Contour Concealer",
+                    price: 69,
+                    image: "",
+                    brand: {
+                        id: "",
+                        name: "Ulta Beauty",
+                    },
+                },
             },
-        },
+            {
+                id: "1",
+                quantity: 1,
+                product: {
+                    id: "",
+                    name: "Doll Unit",
+                    price: 420,
+                    image: "",
+                    brand: {
+                        id: "",
+                        name: "Initial Cut Hair",
+                    },
+                },
+            },
+            {
+                id: "2",
+                quantity: 1,
+                product: {
+                    id: "",
+                    name: "Kook Board",
+                    price: 420,
+                    image: "",
+                    brand: {
+                        id: "",
+                        name: "Catch Surf",
+                    },
+                },
+            },
+        ],
     },
     {
-        product: {
-            id: "",
-            name: "Shred Board",
-            price: "537",
-            image: "",
-            quantity: "",
-            Brand: {
-                id: "",
-                name: "Catch Surf",
+        title: "liked",
+        products: [
+            {
+                id: "0",
+                product: {
+                    id: "",
+                    name: "Brunch Pony",
+                    price: 29.99,
+                    image: "",
+                    brand: {
+                        id: "",
+                        name: "Initial Cut Hair",
+                    },
+                },
             },
-        },
+            {
+                id: "1",
+                product: {
+                    id: "",
+                    name: "Shred Board",
+                    price: 537,
+                    image: "",
+                    brand: {
+                        id: "",
+                        name: "Catch Surf",
+                    },
+                },
+            },
+        ],
     },
 ];
 
